ef8e2c130855f0e02bafcdad674726be3ea6b55e
Karim BENHAMMOU <karim@benhammou.me>
2021-03-22 18:29:27 +0100

    build proto module

---
+++ proto/index.js
@@ -0,0 +1,35 @@
+const pb = require('./pb_pb');
+const grpc = require('./pb_grpc_pb');
+const grpcjs = require('@grpc/grpc-js');
+const google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js');
+
+class WalletClient {
+    token = ""
+    constructor(target, token) {
+        this.client = new grpc.WalletClient(target, grpcjs.credentials.createInsecure());
+        this.token = token
+    }
+
+    getAccount(callback) {
+        const metadata = new grpcjs.Metadata()
+        metadata.add("auth", this.token)
+
+        this.client.accountGet(new google_protobuf_empty_pb.Empty(), metadata, callback)
+    }
+
+    sendPayout(address, amount, callback) {
+        const payoutRequest = new pb.PayoutReq();
+        payoutRequest.setAddress(address)
+        payoutRequest.setAmount(amount)
+
+        const metadata = new grpcjs.Metadata()
+        metadata.add("auth", this.token)
+
+        this.client.newPayout(payoutRequest, metadata, (err, payoutRequest) => {
+            console.log(err, payoutRequest)
+        })
+    }
+
+}
+
+exports.WalletClient = WalletClient
