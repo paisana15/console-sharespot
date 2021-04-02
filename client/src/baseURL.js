4f7f7d35993cc7db87571cbde496168a196be305
saifulshihab <shihabmd1970@gmail.com>
2021-04-03 00:13:47 +0600

    base URL

--- client/src/baseURL.js
+++ client/src/baseURL.js
@@ -1,2 +1,2 @@
-// export const baseURL = `https://api.dashboard.sharespot.pt:8888`;
-export const baseURL = `http://localhost:5001`;
+export const baseURL = `https://api.dashboard.sharespot.pt:8888`;
+// export const baseURL = `http://localhost:5001`;
