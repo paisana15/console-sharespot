430d284decadc89ede1a23e45bbc35576e7159b6
herculanocosta <80786433+herculanocosta@users.noreply.github.com>
2021-04-05 14:38:39 +0100

    Update AdminController.js

--- server/controller/AdminController.js
+++ server/controller/AdminController.js
@@ -608,14 +608,14 @@ const withdrawalRequestAccept = asyncHandler(async (req, res) => {
                     <xml>
                         <o:OfficeDocumentSettings>
                         <o:AllowPNG/>
-                        <o:PixelsPerInch>96</o:PixelsPerInch>
+                        <o:PixelsPerInch></o:PixelsPerInch>
                         </o:OfficeDocumentSettings>
                     </xml>
                     <![endif]-->
                     <meta charset="UTF-8">
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <title>*|MC:SUBJECT|*</title>
+                    <title>Recebeu um pagamento de Sharespot Portugal</title>
                     
                 <style type="text/css">
                 p{
