6df1b8dc20bb129bd597c5c5fc5b82eb7ff22fdd
saifulshihab <shihabmd1970@gmail.com>
2021-11-09 21:34:29 +0600

    chore

--- client/src/baseURL.js
+++
@@ -1,2 +0,0 @@
-export const baseURL = `https://api.dashboard.sharespot.pt:8888`;
-// export const baseURL = `http://localhost:5001`;
---
+++ client/src/constants/index.js
@@ -0,0 +1,2 @@
+export const baseURL = `https://api.dashboard.sharespot.pt:8888`;
+// export const baseURL = `http://localhost:5001`;
