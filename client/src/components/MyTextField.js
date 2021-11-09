6f75ac0676e806d8261d2874c74752ad3ba420cd
saifulshihab <shihabmd1970@gmail.com>
2021-11-09 22:30:53 +0600

    text input layout change

--- client/src/components/MyTextField.js
+++ client/src/components/MyTextField.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { FormLabel } from '@chakra-ui/form-control';
 import { Input } from '@chakra-ui/input';
 import { useField } from 'formik';
-import { Stack } from '@chakra-ui/layout';
+import { Box } from '@chakra-ui/layout';
 
 const MyTextField = ({ label, ref, placeholder, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <Stack mb='3' direction={['column', 'row']}>
+    <Box mb='3'>
       <FormLabel style={{ width: '60%' }}>{label}</FormLabel>
       <Input placeholder={placeholder} {...field} {...props} ref={ref} />
       {meta.touched && meta.error ? (
         <div style={{ color: 'red', fontSize: 13 }}>{meta.error}</div>
       ) : null}
-    </Stack>
+    </Box>
   );
 };
 
