a6cff76a485849b9ed5f1f644eae60c0a5b5e19c
saifulshihab <shihabmd1970@gmail.com>
2021-11-12 23:52:20 +0600

    chore

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import theme from './utils/theme';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <Router>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Provider store={store}>
           <App />
         </Provider>
